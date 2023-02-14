c35449527ccfdfb82bf44ffee492eff5857c75a0
andersonlenzjava <105016572+andersonlenzjava@users.noreply.github.com>
2023-02-14 16:33:10 -0300

    Finalização do CRUD de professores

--- src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
@@ -6,6 +6,7 @@ import br.com.xavecoding.regescweb.models.StatusProfessor;
 import br.com.xavecoding.regescweb.repositories.ProfessorRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -108,6 +109,55 @@ public class ProfessorController {
             return new ModelAndView("redirect:/professores");
         }
     }
+
+    @PostMapping("/{id}")
+    public ModelAndView update(@PathVariable Long id, @Valid RequisicaoFormProfessor formNovoProfessor,
+                               BindingResult bindingResult) {
+        // se a validação tiver erros faz esta operação
+        if (bindingResult.hasErrors()) {
+            System.out.println("\n******************** TEM ERROS *****************\n");
+
+            ModelAndView mv = new ModelAndView("professores/edit");
+            // passagem do id do objeto para o caso de entrar no form erro
+            mv.addObject("professorId", id);
+
+            // atributo que é carregado novamente ao entrar na clase de erros
+            mv.addObject("listaStatusProfessor", StatusProfessor.values());
+            return mv;
+
+        }
+        else {
+            Optional<Professor> optionalProfessor = this.professorRepository.findById(id);
+
+            if(optionalProfessor.isPresent()) {
+                Professor professor = formNovoProfessor.toProfessor(optionalProfessor.get());
+                this.professorRepository.save(professor);
+
+                // redireciona para os detalhes do recurso atualizado (da mesma forma quando é criado)
+                return new ModelAndView("redirect:/professores/" + professor.getId());
+
+            }  else {
+                System.out.println("##### NÃO FOI ENCONTRADO O PROFESSOR DE ID " + id + " ####");
+                //poderia fazer uma mensagem de erro, mas redireciona para a listagem
+                return new ModelAndView("redirect:/professores");
+            }
+        }
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        // tratamento do erro para evitar que o usuario saiba que fez algo errado
+        try {
+            this.professorRepository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            System.out.println(e);
+            return "redirect:/professores";
+        }
+        return "redirect:/professores";
+    }
+
+
 
 
     // para o objeto ser encontrado pela view new
